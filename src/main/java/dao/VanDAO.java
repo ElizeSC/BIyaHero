1008b953e44993b22293f5c076923e259a3aa0d1
Daisha Lynn Cabantac <daishalcb@gmail.com>
2026-04-12 17:27:17 +0800

    Add files via upload

--- src/main/java/dao/VanDAO.java
+++ src/main/java/dao/VanDAO.java
@@ -6,6 +6,8 @@ import java.util.List;
 public interface VanDAO {
     void addVan(Van van);
     Van getVanById(int id);
+    Van getVanByPlateNumber(String plateNumber);
+    Van getVanByModel(String model);
     List<Van> getAllVans();
     void updateVanStatus(int id, String status); // e.g., 'Available', 'In Transit', 'Maintenance'
     void deleteVan(int id);
