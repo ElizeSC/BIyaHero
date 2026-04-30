f274046401b14764ecd8fa33cd75351431de3e5f
Elize Capawing <armeliz12345@gmail.com>
2026-04-30 20:49:04 +0800

    added add route logic + ui

---
+++ src/main/java/com/biyahero/controller/RouteBuilderController.java
@@ -0,0 +1,4 @@
+package com.biyahero.controller;
+
+public class RouteBuilderController {
+}
--- src/main/java/com/biyahero/dao/RouteDAO.java
+++ src/main/java/com/biyahero/dao/RouteDAO.java
@@ -1,10 +1,13 @@
 package com.biyahero.dao;
 
 import com.biyahero.model.Route;
+import com.biyahero.model.RouteStop;
 import java.util.List;
 
 public interface RouteDAO {
     Route getRouteById(int id);
     List<Route> getAllRoutes();
     void updateBaseFare(int id, double newFare);
+
+    int saveRouteWithStops(Route route, List<RouteStop> stops);
 }
